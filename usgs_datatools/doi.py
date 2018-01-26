c1b9cfb6d96357f5b675b6878eafbb0d4b34b56b
Tudor <garbuletvt@ornl.gov>
2018-01-25 23:47:36 -0500

    Adds doi_base_url variable for authentication only

--- usgs_datatools/doi.py
+++ usgs_datatools/doi.py
@@ -14,11 +14,11 @@ class DoiSession():
         :param env: optional default is "staging"
         """
         if env == 'production':
-            self._base_doi_url = 'https://www1.usgs.gov/csas/doi/api/csrf'
+            self._base_doi_url = 'https://www1.usgs.gov/csas/doi/'
         if env == 'dev':
-            self._base_doi_url = 'https://www1-dev.snafu.cr.usgs.gov/csas/doi/api/csrf'
+            self._base_doi_url = 'https://www1-dev.snafu.cr.usgs.gov/csas/doi/'
         if env == 'local':
-            self._base_doi_url = 'https://localhost:8443/doi/api/csrf'
+            self._base_doi_url = 'https://localhost:8443/doi/'
         else:
             print('Using Staging Environment\n')
             self._base_doi_url = 'https://www1-staging.snafu.cr.usgs.gov/csas/doi/'
@@ -32,10 +32,10 @@ class DoiSession():
         :param password: Current USGS user password (Acitve Directory).
         """
         # Fetch application cookie for follow requests.
-        cookie_getter = self._session.get(self._base_doi_url, verify=False)
+        self._base_api_url = 'https://localhost:8443/doi/api/csrf'
+        cookie_getter = self._session.get(self._base_api_url, verify=False)
         self._csrf = cookie_getter.headers['X-CSRF-TOKEN']
         self._username = username  # Save username
-        self._base_doi_url = 'https://localhost:8443/doi/'
         response = self._session.post(self._base_doi_url + 'j_spring_security_check', data = {'j_username': self._username, 'j_password': password, '_csrf': self._csrf}, verify = False) # , verify = False
 
         if 'crowd.token_key' not in self._session.cookies:
